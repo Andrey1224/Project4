7e45836a96562a981b8a1837d790f875fa50ad2b
Andrey1224 <nepod77@gmail.com>
2021-07-04 18:34:55 -0400

    finished

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
     var progressView: UIProgressView!
+    var websites = ["apple.com", "3baristasorganic.com"]
+    
     
     
     override func loadView() {
@@ -42,7 +44,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
-        if let url = URL(string: "https://3baristasorganic.com/") {
+        if let url = URL(string: "https://" + websites[0]) {
             webView.load(URLRequest(url: url))
             webView.allowsBackForwardNavigationGestures = true
             // позволяет смахивать лаво право
@@ -54,8 +56,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
     @objc func openTapped() {
         
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "3baristasorganic.com", style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -80,6 +83,22 @@ class ViewController: UIViewController, WKNavigationDelegate {
         if keyPath == "estimatedProgress" {
             progressView.progress = Float(webView.estimatedProgress)
         }
+        
     }
+    
+    
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        if let host = url?.host{
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel)
+    }
+    
 }
-
